40deec5983e409194c1d05fc241e4e2cb2d4c54f
wunderlins <simon.wunderlin@usb.ch>
2016-02-18 09:21:14 +0100

    Added empty project

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name oibTafelApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the oibTafelApp
+ */
+angular.module('oibTafelApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
