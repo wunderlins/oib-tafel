2eb63d1bc5e38b3ce628a17180a41d8a2968c368
wus <swunderlin@gmail.com>
2016-02-21 21:33:11 +0100

    worked on the data loading methods

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 angular.module('oibTafelApp')
 	.factory("oibTafelAppFactory", ['$http', function($http) {
 	return {
@@ -8,6 +9,7 @@ angular.module('oibTafelApp')
 		}
 	};
 }]);
+*/
 
 angular.module('oibTafelApp')
 	.factory("oibTafelAppFactoryBetten", ['$http', function($http) {
@@ -18,6 +20,34 @@ angular.module('oibTafelApp')
 	};
 }]);
 
+angular.module('oibTafelApp')
+	.factory("oibTafelAppFactoryPatienten", ['$http', function($http) {
+	return {
+		list: function(callback){
+			$http.get('data/patienten.json').success(callback);
+		}
+	};
+}]);
+
+/**
+ * OIB Bett object
+ *
+ * contains room nuber, patient and nurse data
+ */
+function Bett(zimmer, bett) {
+	this.id      = zimmer + "B" + bett;
+	this.zimmer  = zimmer;
+	this.bett    = bett;
+	this.patient = null;
+	this.nurse   = null;
+	return this;
+}
+
+function Zimmer(nr) {
+	this.nr = nr;
+	this.betten = [];
+}
+
 
 /**
  * @ngdoc function
@@ -27,13 +57,14 @@ angular.module('oibTafelApp')
  * Controller of the oibTafelApp
  */
 angular.module('oibTafelApp')
-  .controller('MainCtrl', function ($scope, oibTafelAppFactory, oibTafelAppFactoryBetten) {
+  .controller('MainCtrl', function ($scope, $timeout,
+                                            oibTafelAppFactoryBetten,
+                                            oibTafelAppFactoryPatienten) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-    
     
     // pull data from:
     // - PEP
@@ -41,15 +72,65 @@ angular.module('oibTafelApp')
     // - tafelDB (a table currently stored in mdsi)
     //$scope.patienten = oibTafelAppFactory.getPatienten();
     //console.log($scope.patienten);
-
-		oibTafelAppFactory.list(function(oibTafelAppFactory) {
-			$scope.patienten = oibTafelAppFactory;
-		});
 		
+		// hold all rooms with all patients
+		$scope.data = [];
 		
+		// holds final data structure for the template
+		$scope.betten_data = [];
+		
+		// a list of beds in the form of <room#>B<bed#>
+		$scope.betten = [];
+		$scope.zimmer = [];
+		
+		// keep track of asynchronous calls
+		$scope.loaded = {
+			patienten: false,
+			zimmer: false			
+		};
+		
+		// fetch data
 		oibTafelAppFactoryBetten.list(function(oibTafelAppFactoryBetten) {
-			$scope.zimmer = oibTafelAppFactoryBetten.zimmer;
+			$scope.zimmerData = oibTafelAppFactoryBetten.zimmer;
+			$scope.loaded.zimmer = true;
+			
+			for (var z in $scope.zimmerData) {
+				for (var b in $scope.zimmerData[z].betten) {
+					if ($scope.zimmer.indexOf($scope.data[$scope.zimmerData[z].nr]) !== -1) {
+						$scope.zimmer.push($scope.data[$scope.zimmerData[z].nr]);
+					}
+					$scope.betten.push($scope.zimmerData[z].nr+"B"+$scope.zimmerData[z].betten[b]);
+					var bett = new Bett($scope.zimmerData[z].nr, $scope.zimmerData[z].betten[b]);
+					//$scope.zimmer.betten[b] = bett;
+					//$scope.data.push(bett);
+				}
+			}
+			console.log($scope.zimmer);
 		});
 		 
+		oibTafelAppFactoryPatienten.list(function(oibTafelAppFactoryPatienten) {
+			$scope.patienten = oibTafelAppFactoryPatienten.patienten;
+			//console.log(oibTafelAppFactoryPatienten.patienten)
+			$scope.loaded.patienten = true;
+		});
 		
+		// glue beds and patients together, the key is patienten[n].PD_Bett
+		$timeout(function () {
+			if ($scope.loaded.patienten && $scope.loaded.zimmer) {
+				//console.log($scope.loaded.patienten);
+				
+				for (var e in $scope.patienten) {
+					var bett = $scope.patienten[e].PD_Bett;
+					var ix = $scope.betten_data.indexOf(bett, 0);
+					if (ix !== -1) {
+						//console.log(ix);
+						$scope.betten_data[ix].patient = $scope.patienten[e]; 
+					}
+				}
+				//console.log($scope.data);
+				$scope.loaded.all = 1;
+				return true;
+			}
+		}, 100, this); 
+
   });
